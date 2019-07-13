99519958ae87df2676719b83c4823e506b6f32aa
shobhitbhargava0809 <shobhitbhargava0809@gmail.com>
2019-07-14 03:52:14 +0530

    frontend+backend

---
+++ Fit/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database:"mongodb://localhost:27017/IITM",    //here IITM is the name of database you want to connect to 
+    secret: "yoursecret"     //your secret which makes the database unique
+}
